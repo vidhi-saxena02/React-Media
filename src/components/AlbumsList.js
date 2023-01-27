5e057fa30c882de3073f7baa2c41c870bf8aafff
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-01-27 19:50:47 +0530

    Add albums using RTK query functionality added

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,44 @@
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+
 function AlbumsList({ user }) {
-  return <div>Albums for {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton className="h-10 w-full" times={3} />;
+  } else if (error) {
+    content = <div>Error loading albums...</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of Photos in the
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div className="m-3 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>
+          + Add Album
+        </Button>
+      </div>
+
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
---
+++ src/store/apis/albumsApi.js
@@ -0,0 +1,55 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { faker } from "@faker-js/faker";
+
+const albumsApi = createApi({
+  reducerPath: "albums",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3005",
+  }),
+  endpoints(builder) {
+    return {
+      addAlbum: builder.mutation({
+        invalidatesTags: (result, error, user) => {
+          return [
+            {
+              type: "Album",
+              id: user.id,
+            },
+          ];
+        },
+        query: (user) => {
+          return {
+            url: "albums",
+            method: "POST",
+            body: {
+              userId: user.id,
+              title: faker.commerce.productName(),
+            },
+          };
+        },
+      }),
+      fetchAlbums: builder.query({
+        providesTags: (result, error, user) => {
+          return [
+            {
+              type: "Album",
+              id: user.id,
+            },
+          ];
+        },
+        query: (user) => {
+          return {
+            url: "/albums",
+            params: {
+              userId: user.id,
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
+export { albumsApi };
