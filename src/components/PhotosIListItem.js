b7360ce5955661c6df7465147607b5fb833bcf98
vidhi-saxena02 <vidhisaxena02@yahoo.com>
2023-01-29 00:37:26 +0530

    delete photos and tags functionality added

--- src/components/PhotosIListItem.js
+++ src/components/PhotosIListItem.js
@@ -1,7 +1,18 @@
+import { useRemovePhotoMutation } from "../store";
+import { GoTrashcan } from "react-icons/go";
+
 function PhotosListItem({ photo }) {
+  const [removePhoto] = useRemovePhotoMutation();
+
+  const handleRemovePhoto = () => {
+    removePhoto(photo);
+  };
   return (
-    <div className="relative m-2">
+    <div className="relative m-2 cursor-pointer " onClick={handleRemovePhoto}>
       <img className="h-20 w-20 " src={photo.url} alt="random/pic" />
+      <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+        <GoTrashcan className="text-3xl" />
+      </div>
     </div>
   );
 }
